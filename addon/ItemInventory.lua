94e9c1e242d8e53077320fa97ea015933dd7f8bf
datagutten <datagutten@datagutten.net>
2023-08-20 13:06:29 +0200

    Update for BM-utils 2 and new LibInventory

--- addon/ItemInventory.lua
+++ addon/ItemInventory.lua
@@ -8,13 +8,18 @@ _G['ItemInventoryAddon'] = addon
 
 local minor
 ---@type BMUtils
-addon.utils, minor = _G.LibStub('BM-utils-1')
-assert(minor >= 6, ('BMUtils 1.6 or higher is required, found 1.%d'):format(minor))
-assert(_G['LibInventory'].main, 'LibInventory not loaded')
+addon.utils, minor = _G.LibStub('BM-utils-2')
+--assert(minor >= 6, ('BMUtils 1.6 or higher is required, found 1.%d'):format(minor))
 
----@type LibInventory
-addon.inventory = _G['LibInventory']
-addon.characterInfo = _G['CharacterData']
+---TODO: LibInventory version check
+
+---@type LibInventoryAce
+local lib_inventory = _G.LibStub("AceAddon-3.0"):GetAddon('LibInventoryAce')
+---@type LibInventoryLocations
+local inventory = lib_inventory:GetModule('LibInventoryLocations')
+---@type LibInventoryCharacter
+local characters = lib_inventory:GetModule('LibInventoryCharacter')
+
 local is_classic = _G.WOW_PROJECT_ID ~= _G.WOW_PROJECT_MAINLINE
 
 local frame = _G.CreateFrame("Frame"); -- Need a frame to respond to events
@@ -22,12 +27,7 @@ local frame = _G.CreateFrame("Frame"); -- Need a frame to respond to events
 
 frame:SetScript("OnEvent", function(_, event, arg1)
     if event == "ADDON_LOADED" and arg1 == "ItemInventory" then
-        --@debug
-        addon.utils:printf("%s loaded", addonName)
-        --@end-debug
-
-        local character = addon.characterInfo:current()
-        character:save()
+        characters:save()
     end
 end);
 frame:RegisterEvent("ADDON_LOADED"); -- Fired when saved variables are loaded
@@ -36,26 +36,27 @@ addon.location_color = 'ffffffff'
 addon.quantity_color = 'ff00ff00'
 
 function addon:itemCountTooltip(itemID)
-    for character, locations in pairs(self.inventory.main:getItemLocation(itemID)) do
+    for character, locations in pairs(inventory:getItemLocation(itemID)) do
         if next(locations) ~= nil then
             local sum = 0
             local location_strings = {}
-            local name, realm = self.utils:SplitCharacterString(character)
+            local guildBankString
+            local name, realm = self.utils.character.splitCharacterString(character)
             for location, quantity in pairs(locations) do
-                location = self.inventory.main:locationName(location)
-                local location_string = self.utils:colorize(location .. ': ', self.location_color)
-                local quantity_string = self.utils:colorize(quantity, self.quantity_color)
+                location = inventory:locationName(location)
+                local location_string = self.utils.text.colorize(location .. ': ', self.location_color)
+                local quantity_string = self.utils.text.colorize(quantity, self.quantity_color)
 
                 sum = sum + quantity
                 table.insert(location_strings, location_string .. quantity_string)
             end
 
             ---@type CharacterData
-            local characterInfo = _G['CharacterData']:load(realm, name)
+            local characterInfo = characters:load(realm, name)
             local right_text = table.concat(location_strings, '||')
             if characterInfo then
                 local color = characterInfo:color()
-                local icon = self.utils:sprintf('|A:raceicon-%s-%s:12:12|a',
+                local icon = ('|A:raceicon-%s-%s:12:12|a'):format(
                         characterInfo.raceFile,
                         characterInfo:genderString()
                 )
@@ -75,7 +76,7 @@ local function tooltip_handler(self)
     if not link then
         return
     end
-    local itemID = addon.utils:ItemIdFromLink(link)
+    local itemID = addon.utils.itemIdFromLink(link)
     addon:itemCountTooltip(itemID)
 end
 
